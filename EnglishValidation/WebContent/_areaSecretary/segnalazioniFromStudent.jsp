4794ee2a6590aec863118f23f8796224f3b8cff8
herme <herme@DESKTOP-LV62E5H>
2019-12-30 11:16:49 +0100

    -AGGIUNTA DEL MODEL SEGNALAZIONE IN MODEL -AGGIUNTA DEL CONTROLLER SEGNAAZLIONEDATABASE PER LE QUERY DELLE SEGNALAZIONI (QUERY DA CONTROLLARE!) - AGGIORNAMENTO DELLE JSP DI SEGNALAZIONI

--- EnglishValidation/WebContent/_areaSecretary/segnalazioniFromStudent.jsp
+++ EnglishValidation/WebContent/_areaSecretary/segnalazioniFromStudent.jsp
@@ -1,12 +1,91 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="ISO-8859-1" import="controller.CheckSession"%>
+<%@ page
+	import="controller.SegnalazioneDatabase,model.Segnalazione,java.util.*,model.Request,controller.DbConnection,controller.ServletAdmin,java.sql.ResultSet,java.sql.Statement"%>
+
+<%
+	String pageName = "segnalazioniFromStudent.jsp";
+	String pageFolder = "_areaAdmin";
+	ArrayList<Segnalazione> segnalazioni = new ArrayList<Segnalazione>();
+	SegnalazioneDatabase datb=new SegnalazioneDatabase();
+	segnalazioni=datb.getSegnalazioniFromStudent();
+			
+	
+	
+	CheckSession ck = new CheckSession(pageFolder, pageName, request.getSession());
+	if(!ck.isAllowed()){
+	  response.sendRedirect(request.getContextPath()+ck.getUrlRedirect());  
+	}
+%>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<jsp:include page="/partials/head.jsp" />
 </head>
-<body>
+
+<body onLoad="showData()">
+	<div class="page-wrapper">
+
+		<!-- Preloader -->
+		<!--  <div class="preloader"></div> -->
+
+
+		<jsp:include page="/partials/header.jsp">
+			<jsp:param name="pageName" value="<%= pageName %>" />
+			<jsp:param name="pageFolder" value="<%= pageFolder %>" />
+		</jsp:include>
+
+
+		<div class="sidebar-page-container basePage viewRequestAdmin">
+			<div class="auto-container">
+				<div class="row clearfix">
+					<div class="content-side col-lg-12 col-md-12 col-sm-12 col-xs-12">
+						<div class="content ">
+							<div class="news-block-seven">
+								<table id="adminTable" class="display data-results table table-striped table-hover table-bordered">
+									<thead>
+										<tr>
+											<th class="text-center">ID_REPORT</th>
+											<th class="text-center">HEADER</th>
+											<th class="text-center">VISUALIZZA</th>
+										
+										</tr>
+									</thead>
+									<tbody id="bodyAdminTable">
+                                    <%
+							//int [] itera;
+								//itera = new int [100];
+
+								for (int i = 0; i < segnalazioni.size(); i++) {
+
+									
+						%>
+						<tr>
+							<td><%= segnalazioni.get(i).getIdReport() %></td>
+							<td><%= segnalazioni.get(i).getHead() %></td>
+							<td><button onClick="" > Visualizza</button></td>
+						</tr>
+						
+						<%
+							}
+						%>
+									</tbody>
+								</table>
+								
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<jsp:include page="/partials/footer.jsp" />
+	</div>
+	<!--End pagewrapper-->
+
+	<jsp:include page="/partials/includes.jsp" />
+	
 
 </body>
 </html>
---
+++ EnglishValidation/src/controller/SegnalazioneDatabase.java
@@ -0,0 +1,99 @@
+package controller;
+import java.sql.Connection;
+import java.sql.Date;
+import java.util.ArrayList;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import model.Segnalazione;;
+public class SegnalazioneDatabase {
+	public SegnalazioneDatabase() {
+		
+	}
+	
+	public ArrayList<Segnalazione> getSegnalazioniFromStudent(){
+		 PreparedStatement stmt = null;
+		 ArrayList<Segnalazione> segnalazioni = new ArrayList<Segnalazione>();
+		 Connection conn = new DbConnection().getInstance().getConn();
+		 String sql = "";
+		 if( conn != null ) {
+			 try {
+				 sql = 
+						 "SELECT Report.ID_REPORT, Report.Serial, Report.Body, Report.Head"
+						 + " FROM Report "
+						 + "INNER JOIN SEND_R AS S ON Report.ID_REPORT = S.ID_REPORT "
+						 + "INNER JOIN USER AS U ON S.E-MAIL = U.E-MAIL "
+						 + "WHERE S.USER-TYPE = '1'"; //USER-TYPE DELLO STUDENTE DA DEFINIRE
+				 stmt = conn.prepareStatement(sql);
+			       
+		          ResultSet r = stmt.executeQuery();
+		          if (r.wasNull()) {
+		        	  System.out.println("Errore nell'esecuzione della Query");
+		          } else {
+		        	  while(r.next()) {
+		        		  int id_report = r.getInt("ID_REPORT");
+		        		  int serial = r.getInt("SERIAL");
+		        		  String body = r.getString("BODY");
+		        		  String head = r.getString("HEAD");
+		        		  Segnalazione s = new Segnalazione();
+		        		  s.setBody(body);
+		        		  s.setHead(head);
+		        		  s.setIdReport(id_report);
+		        		  s.setSerial(serial);
+		        		  segnalazioni.add(s);
+		        		  }
+		          }
+		          conn.commit();
+				 
+			 }catch(Exception e) {
+				 System.out.println(e.getMessage());
+			 }
+		 }
+
+		 return segnalazioni;
+		
+	}
+	public ArrayList<Segnalazione> getSegnalazioniFromSecretary(){
+		 PreparedStatement stmt = null;
+		 ArrayList<Segnalazione> segnalazioni = new ArrayList<Segnalazione>();
+		 Connection conn = new DbConnection().getInstance().getConn();
+		 String sql = "";
+		 if( conn != null ) {
+			 try {
+				 sql = 
+						 "SELECT Report.ID_REPORT, Report.Serial, Report.Body, Report.Head"
+						 + " FROM Report "
+						 + "INNER JOIN SEND_R AS S ON Report.ID_REPORT = S.ID_REPORT "
+						 + "INNER JOIN USER AS U ON S.E-MAIL = U.E-MAIL "
+						 + "WHERE S.USER-TYPE = '2'"; //USER-TYPE DELLA SEGRETERIA DA DEFINIRE
+				 stmt = conn.prepareStatement(sql);
+			       
+		          ResultSet r = stmt.executeQuery();
+		          if (r.wasNull()) {
+		        	  System.out.println("Errore nell'esecuzione della Query");
+		          } else {
+		        	  while(r.next()) {
+		        		  int id_report = r.getInt("ID_REPORT");
+		        		  int serial = r.getInt("SERIAL");
+		        		  String body = r.getString("BODY");
+		        		  String head = r.getString("HEAD");
+		        		  Segnalazione s = new Segnalazione();
+		        		  s.setBody(body);
+		        		  s.setHead(head);
+		        		  s.setIdReport(id_report);
+		        		  s.setSerial(serial);
+		        		  segnalazioni.add(s);
+		        		  }
+		          }
+		          conn.commit();
+				 
+			 }catch(Exception e) {
+				 System.out.println(e.getMessage());
+			 }
+		 }
+
+		 return segnalazioni;
+		
+	}
+}
