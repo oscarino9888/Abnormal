d445537b4a602f8286606343836e5b4c99f1ffcf
herme <herme@DESKTOP-LV62E5H>
2020-01-10 20:01:35 +0100

    Creazione dello script per crazioneSegnalazione che invia richiesta post alla ServletSegnalazione per la creazione della prima segnalazione

---
+++ EnglishValidation/WebContent/js/pages/scripts_creaSegnalazione.js
@@ -0,0 +1,48 @@
+$(document)
+		.ready(
+				function() {
+
+					$(document)
+							.on(
+									'submit',
+									'#form-invia',
+									function(e) {
+										var testo_msg = $('#testo').val();
+										var email = $('#email').val();
+											$
+													.ajax({
+														url : absolutePath
+																+ "/ServletSegnalazione",
+														type : "POST",
+														dataType : 'JSON',
+														async : false,
+														data : {
+															"testo" : testo,
+															"email" : email,
+															"flag" : 1
+															
+														},
+														success : function(msg) {
+															if (!msg.result) {
+																showAlert(
+																		1,
+																		msg.error);
+															} else {
+																showAlert(
+																		0,
+																		msg.content);
+
+																setTimeout(
+																		function() {
+																			window.location.href = msg.redirect;
+																		}, 2000);
+															}
+														},
+														error : function(msg) {
+															showAlert(1,
+																	"Impossibile Recuperare i dati.");
+														}
+													});
+									});
+
+				});
