623905cedadc2c40f280023b369917d468bd9c37
herme <herme@DESKTOP-LV62E5H>
2020-01-11 18:34:12 +0100

    AGGIUNTA DELLA RICHIESTA PER L'AGGIUNTA DELLA SEGNALAZIONE COME RISPOSTA

--- EnglishValidation/WebContent/js/pages/scripts_creaSegnalazione.js
+++ EnglishValidation/WebContent/js/pages/scripts_creaSegnalazione.js
@@ -46,3 +46,51 @@ $(document)
 									});
 
 				});
+$(document)
+.ready(
+		function() {
+
+			$(document)
+					.on(
+							'submit',
+							'#form-risposta',
+							function(e) {
+								var testo_msg = $('#testo-risposta').val();
+								var email = $('#email-risposta').val();
+									$
+											.ajax({
+												url : absolutePath
+														+ "/ServletSegnalazione",
+												type : "POST",
+												dataType : 'JSON',
+												async : false,
+												data : {
+													"testo" : testo,
+													"email" : email,
+													"flag" : 1
+													
+												},
+												success : function(msg) {
+													if (!msg.result) {
+														showAlert(
+																1,
+																msg.error);
+													} else {
+														showAlert(
+																0,
+																msg.content);
+
+														setTimeout(
+																function() {
+																	window.location.href = msg.redirect;
+																}, 2000);
+													}
+												},
+												error : function(msg) {
+													showAlert(1,
+															"Impossibile Recuperare i dati.");
+												}
+											});
+							});
+
+		});
